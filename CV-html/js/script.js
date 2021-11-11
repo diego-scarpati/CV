eae018aa95b4efec6563a59a75366a1c970b602c
Diego Scarpati <diegoscarpati13@gmail.com>
2021-11-11 15:18:45 -0300

    Trabajo en Contact

--- CV-html/js/script.js
+++ CV-html/js/script.js
@@ -1,8 +1,17 @@
 let pixelGlasses = document.getElementById('pixel');
 let contact = document.getElementById('contact');
+let address = document.getElementById('address');
+let map = document.getElementById('map');
+let closeMap = document.getElementById('close');
 contact.addEventListener('mouseover', ()=>{
     pixelGlasses.style.display = 'initial'
 })
 contact.addEventListener('mouseout', ()=>{
     pixelGlasses.style.display = 'none'
+})
+address.addEventListener('click', ()=>{
+    map.style.display = 'flex'
+})
+closeMap.addEventListener('click', ()=>{
+    map.style.display = 'none'
 })

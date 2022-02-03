7c3dc4e35f038707d0dac19e3cc708ee8acb3713
Diego Scarpati <diegoscarpati13@gmail.com>
2022-02-03 16:06:08 -0300

    Jquery + certificados responsive

--- CV-html/js/script.js
+++ CV-html/js/script.js
@@ -1,60 +1,70 @@
 // Make pixel glasses appear when hovering Contact
-let pixelGlasses = document.getElementById('pixel');
-let contact = document.getElementById('contact');
-const minWidth = window.matchMedia('(min-width: 901px)')
-const personal = document.querySelector('#personal')
-const screenSize = mql => {
-    if (mql.matches) {
-        contact.addEventListener('mouseover', ()=>{
-            pixelGlasses.style.display = 'initial'
-        })
-        contact.addEventListener('mouseout', ()=>{
-            pixelGlasses.style.display = 'none'
-        })
-    }
-}
+// let pixelGlasses = document.getElementById('pixel');
+// let contact = document.getElementById('contact');
+// const minWidth = window.matchMedia('(min-width: 901px)')
+// const personal = document.querySelector('#personal')
+// const screenSize = mql => {
+//     if (mql.matches) {
+//         contact.addEventListener('mouseover', ()=>{
+//             pixelGlasses.style.display = 'initial'
+//         })
+//         contact.addEventListener('mouseout', ()=>{
+//             pixelGlasses.style.display = 'none'
+//         })
+//     }
+// }
 // screenSize(minWidth)
 // minWidth.addListener(screenSize)
 // window.addEventListener("resize", screenSize)
 
 // Clickable event to open and close map over address
-let address = document.getElementById('address');
-let map = document.getElementById('map');
-let closeMap = document.getElementById('close');
-address.addEventListener('click', ()=>{
-    map.style.display = 'flex'
+let address = $('#address');
+let map = $('#map-div');
+let closeMap = $('#close');
+address.click( ()=>{
+    map.toggle()
 })
-closeMap.addEventListener('click', ()=>{
-    map.style.display = 'none'
+closeMap.click( ()=>{
+    map.css('display', 'none')
 })
 
-// Clickable event to open and close pdf over Agile
-let agile = document.getElementById('Agile');
-let agileDiv = document.getElementById('Agile-div');
-let agileBtn = document.getElementById('Agile-btn');
-agile.addEventListener('click', ()=>{
-    agileDiv.style.display = 'flex'
+// Clickable event to open and close pdf over JavaScript
+const javaScript = $('#JavaScript');
+const javaScriptDiv = $('#JavaScript-div');
+const javaScriptBtn = $('#JavaScript-btn');
+javaScript.click( ()=>{
+    javaScriptDiv.toggle()
 })
-agileBtn.addEventListener('click', ()=>{
-    agileDiv.style.display = 'none'
+javaScriptBtn.click( ()=>{
+    javaScriptDiv.css('display', 'none')
+})
+// Clickable event to open and close pdf over Agile
+const agile = $('#Agile');
+const agileDiv = $('#Agile-div');
+const agileBtn = $('#Agile-btn');
+agile.click( ()=>{
+    agileDiv.toggle()
+})
+agileBtn.click( ()=>{
+    agileDiv.css('display', 'none')
 })
 // Clickable event to open and close pdf over PyBoot
-let pyBoot = document.getElementById('PyBoot');
-let pyBootDiv = document.getElementById('PyBoot-div');
-let pyBootBtn = document.getElementById('PyBoot-btn');
-pyBoot.addEventListener('click', ()=>{
-    pyBootDiv.style.display = 'flex'
+const pyBoot = $('#PyBoot');
+const pyBootDiv = $('#PyBoot-div');
+const pyBootBtn = $('#PyBoot-btn');
+pyBoot.click( ()=>{
+    pyBootDiv.toggle()
 })
-pyBootBtn.addEventListener('click', ()=>{
-    pyBootDiv.style.display = 'none'
+pyBootBtn.click( ()=>{
+    pyBootDiv.css('display', 'none')
 })
 // Clickable event to open and close pdf over BI
-let bI = document.getElementById('BI');
-let bIDiv = document.getElementById('BI-div');
-let bIBtn = document.getElementById('BI-btn');
-bI.addEventListener('click', ()=>{
-    bIDiv.style.display = 'flex'
+const bI = $('#BI');
+const bIDiv = $('#BI-div');
+const bIBtn = $('#BI-btn');
+bI.click( ()=>{
+    bIDiv.toggle()
 })
-bIBtn.addEventListener('click', ()=>{
-    bIDiv.style.display = 'none'
+bIBtn.click( ()=>{
+    bIDiv.css('display', 'none')
 })
